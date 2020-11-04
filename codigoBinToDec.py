47b84a3fd31a96a04e1fb112794ad115d7f6cc14
kalil0027 <73914617+kalil0027@users.noreply.github.com>
2020-11-04 10:26:41 -0300

    Delete codigoBinToDec.py

--- codigoBinToDec.py
+++
@@ -1,9 +0,0 @@
-def binToDec(binarystring):
-    sum = 0
-    base = 1
-    for i in reversed(list(str(binarystring))):
-        sum = sum + (base * int(i))
-        base = base * 2
-    print(sum)
-
-#binToDec()
